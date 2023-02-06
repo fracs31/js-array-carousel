42eaf9221a0560d39fca4f9a1c74e765591fae07
fracs31 <pist.fracs95@gmail.com>
2023-02-06 15:21:29 +0100

    START javascript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let images = ["../img/01.jpg", "../img/02.jpg", "../img/03.jpg", "../img/04.jpg", "../img/05.jpg"] //array con le immagini del carosello
+let btnLeft = document.getElementById("btn-left"); //bottone per andare indietro con le immagini del carosello
+let btnRight = document.getElementById("btn-right"); //bottone per andare avanti con le immagini del carosello
+
+//Quando viene premuto il tasto per andare avanti con le immagini del carosello
+btnRight.addEventListener("click", function() {
+    alert("Ciao");
+});
